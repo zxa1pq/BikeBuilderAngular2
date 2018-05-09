a99fd18b042c20ca54948ae7f49d2ababf513a67
ZXA1PQ <zxa1p1@gmail.com>
2018-05-09 10:50:33 +0200

    added homecomponent

--- BikeBuilderAngular/src/app/app.component.ts
+++ BikeBuilderAngular/src/app/app.component.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container"  style="text-align:center; padding-top:3%;">
   <router-outlet></router-outlet>
-  </div>
   `,
   styleUrls: ['./app.component.css']
 })
